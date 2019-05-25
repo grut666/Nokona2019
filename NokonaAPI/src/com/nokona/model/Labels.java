95766f643e09614474bd227d8549fc6ed90b398e
Mark Waggoner <grut@grut-computing.com>
2019-05-25 09:35:37 -0500

    Adding printer test

--- NokonaAPI/src/com/nokona/model/Labels.java
+++ NokonaAPI/src/com/nokona/model/Labels.java
@@ -1,13 +1,17 @@
 package com.nokona.model;
 
 public class Labels {
+	private static final int DEFAULT_PAGE_QUANTITY = 1;
 	private String labels;
 
 	public Labels() {
 		super();
 	}
 	public Labels(Employee emp) {
-		generateLabels(emp);
+		generateLabels(emp, DEFAULT_PAGE_QUANTITY);
+	}
+	public Labels(Employee emp, int page_quantity) {
+		generateLabels(emp, page_quantity);
 	}
 	public String getLabels() {
 		return labels;
@@ -15,8 +19,8 @@ public class Labels {
 	public void setLabels(String labels) {
 		this.labels = labels;
 	}
-	protected void generateLabels(Employee emp) {
+	protected void generateLabels(Employee emp, int page_quantity) {
 		StringBuilder sb = new StringBuilder("");
-		labels = "Label Stuff Will Be Here";
+		labels = "Label Stuff Will Be Here.  Quantity: " + page_quantity;
 	}
 }

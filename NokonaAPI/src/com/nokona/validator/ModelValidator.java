9f02a3a5b6c778f3c87e40dc078070d9db24f3c2
Mark Waggoner <grut@grut-computing.com>
2020-01-25 10:14:25 -0600

    Updated a possibly faulty modelValidator

--- NokonaAPI/src/com/nokona/validator/ModelValidator.java
+++ NokonaAPI/src/com/nokona/validator/ModelValidator.java
@@ -10,7 +10,8 @@ import com.nokona.model.Model;
 
 // No duplicate modelID
 public class ModelValidator {
-	private static PreparedStatement updatePS;
+	private static PreparedStatement updatePS1;
+	private static PreparedStatement updatePS2;
 	private static PreparedStatement addPS;
 
 
@@ -42,20 +43,41 @@ public class ModelValidator {
 
 	public static String validateUpdate(Model modelIn, Connection conn) {
 		String errors = "";
-		if (updatePS == null) {
+//
+//		if (updatePS1 == null) {
+//			try {
+//				updatePS1 = conn.prepareStatement(
+//						"Select Model.key from Model where Model.key = ?");
+//			} catch (SQLException e) {
+//				errors += e.getMessage() + "\n";
+//			}	
+//		} else {
+//				try {
+//					updatePS1.setLong(1, modelIn.getKey());
+//					ResultSet rs = updatePS1.executeQuery();
+//					if (! rs.next()) {
+//						errors += "Update: Key doesn't exist\n";
+//					}
+//					rs.close();
+//				} catch (SQLException e) {
+//					errors += e.getMessage() + "\n";
+//				}
+//				
+//			}
+		if (updatePS2 == null) {
 			try {
-				updatePS = conn.prepareStatement(
+				updatePS2 = conn.prepareStatement(
 						"Select Model.key from Model where Model.key <> ? and isDeleted = 'F' and modelID = ?");
 			} catch (SQLException e) {
 				errors += e.getMessage() + "\n";
 			}	
 		} else {
 				try {
-					updatePS.setLong(1, modelIn.getKey());
-					updatePS.setString(2, modelIn.getModelId());
-					ResultSet rs = updatePS.executeQuery();
+					updatePS2.setLong(1, modelIn.getKey());
+					updatePS2.setString(2, modelIn.getModelId());
+					ResultSet rs = updatePS2.executeQuery();
 					if (rs.next()) {
-						errors += "Update: Model ID already in use\n";
+						errors += "Update: Model ID would be a duplicate\n";
 					}
 					rs.close();
 				} catch (SQLException e) {

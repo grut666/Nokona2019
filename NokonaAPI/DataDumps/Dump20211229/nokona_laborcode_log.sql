d57412a5a3922818b50389e98a9ebd5904ac4a86
mark <grut@grut-computing.com>
2021-12-29 14:08:30 -0600

    new Data Dump and configure for Nokona server access

---
+++ NokonaAPI/DataDumps/Dump20211229/nokona_laborcode_log.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: nokona
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `laborcode_log`
+--
+
+DROP TABLE IF EXISTS `laborcode_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `laborcode_log` (
+  `LaborCode` int(11) DEFAULT NULL,
+  `Description` varchar(20) DEFAULT NULL,
+  `LaborRate` double DEFAULT NULL,
+  `TheKey` int(11) DEFAULT NULL,
+  `TransactionDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `TransactionType` char(1) DEFAULT NULL,
+  `ProcessedToAccess` char(1) DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `laborcode_log`
+--
+
+LOCK TABLES `laborcode_log` WRITE;
+/*!40000 ALTER TABLE `laborcode_log` DISABLE KEYS */;
+INSERT INTO `laborcode_log` VALUES (3,'STAMPING',13.2,3,NULL,NULL,'N'),(1,'EMBROIDERYYYY',10.22,33,'2021-12-21 09:32:23','U','N'),(1,'EMBROIDER',10.22,33,'2021-12-21 09:32:33','U','N'),(1,'EMBROIDERY',10.22,33,'2021-12-21 09:34:52','U','N'),(1,'EMBROIDERY',10.22,33,'2021-12-21 09:35:06','U','N'),(1,'EMBROIDERYXXX',10.22,33,'2021-12-21 09:35:18','U','N');
+/*!40000 ALTER TABLE `laborcode_log` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-29 14:06:29

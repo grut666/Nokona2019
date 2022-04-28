732030563339acd42f8961fbdccc2860114f5fea
mark <grut@grut-computing.com>
2022-04-28 14:23:36 -0500

    Added missing JobType 'C'

--- NokonaAPI/src/com/nokona/enums/JobType.java
+++ NokonaAPI/src/com/nokona/enums/JobType.java
@@ -3,7 +3,7 @@ package com.nokona.enums;
 public enum JobType {
 	BASEBALL("B"), SOFTBALL("S"), OTHER("O"), UNKNOWN("U"), REPAIR("R"), CUSTOM("C"),
 	CUSTOM_BASEBALL("CB"), CUSTOM_SOFTBALL("CS"), CUSTOM_OTHER("CO"), 
-			B("B"), S("S"), O("O"), CB("CB"), CS("CS"), CO("CO"), R("R");
+			C("C"), B("B"), S("S"), O("O"), CB("CB"), CS("CS"), CO("CO"), R("R");
 	private String jobType;
 
 	JobType(String jobType) {
